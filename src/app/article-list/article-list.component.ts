dab246c1cd2c8c0a16bef93eba016b1ee46e617e
Srdjan Damnjanovic <srdjan.damnjanovic@msg-global.com>
2021-05-09 01:45:43 +0200

    medivated: removed commented code

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Entry, AssetCollection } from 'contentful';
 import { Observable } from 'rxjs';
@@ -9,26 +9,14 @@ import { ContentfulService } from '../contentful.service';
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss'],
 })
-export class ArticleListComponent implements OnInit {
-  // articles: Entry<any>[] = [];
-  // images: Asset[] = [];
-
+export class ArticleListComponent {
   contents$: Observable<Entry<any>[]> = this.contentfulService.getContent();
-  images$: Observable<AssetCollection> = this.contentfulService.getAssets();
+  // images$: Observable<AssetCollection> = this.contentfulService.getAssets();
 
   constructor(
     private router: Router,
     private contentfulService: ContentfulService
   ) {}
-
-  ngOnInit(): void {
-    // kao obican promise
-    // this.contentfulService
-    //   .getContent()
-    //   .then(articles => (this.articles = articles));
-    // kao obican promise
-    // this.contentfulService.getAssets().then(img => (this.images = img.items));
-  }
 
   goToArticlePage(articleId): void {
     this.router.navigate(['/article', articleId]);
